16a5754574cc1243fae2be31ab8e2409cae95cdc
Nathan Schrader <nathanschrader@icloud.com>
2020-10-20 11:49:05 -0700

    Add colors button  project using jquery
    Co-author-by: Tyler Sinks <tyler.sinks@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("p").click(function() {
+//     $(".walrus-showing").show();
+//   });
+// });
+
+
+// $(document).ready(function() {
+//   $("p").click(function() {
+//     $(".walrus-showing").show();
+//     $(".walrus-hidden").hide();
+//   });
+// });
+
+/*$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").slideToggle();
+    // $("#walrus-hidden").toggle();
+  });
+});
+*/
